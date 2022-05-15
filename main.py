cc9f48a7d691e71060d3d021940f561cc3581a7f
Massimo Sandretti <massimo.sandretti@gmail.com>
2022-05-15 02:07:43 +0200

    UI implemented & flag method & game over method

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Minesweeper')
 clock = pygame.time.Clock()
 pygame.font.init()
-font = pygame.font.SysFont('arial',10)
+font = pygame.font.SysFont('arial',30)
 
-game = Minesweeper((0,0),font)
+game = Minesweeper((100,100), 50, 3,font)
 
 running = True
 
@@ -18,8 +18,21 @@ while running:
     screen.fill(bgcolor)
 
     for event in pygame.event.get():
-        pass
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                clicked_cell = game.get_cell_by_pos(event.pos) 
+                if clicked_cell != None:
+                    game.reveal(clicked_cell)
 
-    
+            elif event.button == 3:
+                clicked_cell = game.get_cell_by_pos(event.pos) 
+                if clicked_cell != None:
+                    game.flag(clicked_cell)
+                pass
+
+    if game.check_gameover():
+        print("game over")
+
+    game.show(screen)
     pygame.display.update()
     clock.tick(60)
--- minesweeper.py
+++ minesweeper.py
@@ -2,9 +2,11 @@ import pygame
 import random
 
 class Minesweeper:
-    def __init__(self, pos, font, rows=9, cols=9, bombs=10):
+    def __init__(self, pos,cell_size,margin, font, rows=9, cols=9, bombs=10):
         self.__pos = pos
         self.__font = font
+        self.__cell_size = cell_size
+        self.__margin = margin
         self.game_settings(rows=rows,cols=cols,bombs=bombs)
 
     def game_settings(self,*, rows=None, cols=None, bombs=None):
@@ -25,8 +27,11 @@ class Minesweeper:
         self.new_game()
 
     def new_game(self):
+        self.__gameover = False
+
         self.__board = [[0 for _ in range(self.__cols)] for _ in range(self.__rows)]
-        self.__revealed = []
+        self.__revealed = [[False for _ in range(self.__cols)] for _ in range(self.__rows)]
+        self.__flagged = [[False for _ in range(self.__cols)] for _ in range(self.__rows)]
         for _ in range(self.__bombs):
             j = random.randrange(0,self.__cols)
             i = random.randrange(0,self.__rows)
@@ -50,19 +55,67 @@ class Minesweeper:
                 print(self.__board[i][j], end=' ')
             print()
         
-    def reveal(self, cell):
+    def flag(self, cell):
         i,j = cell
         if self.__revealed[i][j]:
+            return
+        
+        self.__flagged[i][j] = not self.__flagged[i][j]
+
+    def reveal(self, cell):
+        i,j = cell
+        if self.__revealed[i][j] or self.__flagged[i][j]:
             return
 
         self.__revealed[i][j] = True
 
-        if self.__board[i][j] == 0:
+        if self.__board[i][j] == -1:
+            self.__gameover = True
+        elif self.__board[i][j] == 0:
             for k in range(max(0,i-1), min(self.__rows,i+2)):
                 for l in range(max(0,j-1), min(self.__cols,j+2)):
-                    if self.__board[k][l] == 0 and (k,l) != (i,j):
+                    if (k,l) != (i,j):
                         self.reveal((k,l))
-            
+
+    def get_cell_by_pos(self, pos):
+        rx,ry = pos
+        rx-=self.__pos[0]
+        ry-=self.__pos[1]
+
+        i,j = ry//(self.__cell_size+self.__margin), rx//(self.__cell_size+self.__margin)
+        fx,fy = j*(self.__cell_size+self.__margin)+self.__cell_size,i*(self.__cell_size+self.__margin)+self.__cell_size
+
+        if rx >= fx or ry >= fy:
+            return None
+        if i < 0 or j < 0 or i >= self.__rows or j >= self.__cols:
+            return None
+        return (i,j)
+
+    def check_gameover(self):
+        return self.__gameover
+
+    def show(self, screen):
+        pygame.draw.rect(screen, (255,255,255), pygame.Rect(self.__pos, (self.__cols*self.__cell_size + (self.__cols+1)*self.__margin, self.__rows*self.__cell_size + (self.__rows+1)*self.__margin)), 0, 5)
+
+        for i in range(self.__rows):
+            for j in range(self.__cols):
+                if self.__revealed[i][j]:
+                    cx,cy = (self.__pos[0]+j*self.__cell_size+(j+1)*self.__margin,self.__pos[1]+i*self.__cell_size+(i+1)*self.__margin)
+                    pygame.draw.rect(screen, (100,100,100), pygame.Rect((cx,cy), (self.__cell_size,self.__cell_size)), 0, 3)
+
+                    if self.__board[i][j] != 0:
+                        text_surface = self.__font.render(str(self.__board[i][j]), False, (230,0,0))
+
+                        r = text_surface.get_rect()
+
+                        dx = (r.width)//2
+                        dy = (r.height)//2
+                        screen.blit(text_surface, (cx+self.__cell_size//2-dx, cy+self.__cell_size//2-dy))
+                elif self.__flagged[i][j]:
+                    pygame.draw.rect(screen, (0,0,0), pygame.Rect((self.__pos[0]+j*self.__cell_size+(j+1)*self.__margin,self.__pos[1]+i*self.__cell_size+(i+1)*self.__margin), (self.__cell_size,self.__cell_size)), 0, 3)
+                    pygame.draw.circle(screen, (255,0,0), (self.__pos[0]+j*self.__cell_size+(j+1)*self.__margin + self.__cell_size//2,self.__pos[1]+i*self.__cell_size+(i+1)*self.__margin + self.__cell_size//2), self.__cell_size//2)
+                else:
+                    pygame.draw.rect(screen, (0,0,0), pygame.Rect((self.__pos[0]+j*self.__cell_size+(j+1)*self.__margin,self.__pos[1]+i*self.__cell_size+(i+1)*self.__margin), (self.__cell_size,self.__cell_size)), 0, 3)
 
 
 
